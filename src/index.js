cf45afd835c3bb09425b5aff3f4cbe9110be4bc3
BrendaGPLIM15 <giovannalopez826@gmail.com>
2021-06-21 12:36:16 -1100

    Primera versión Card

--- src/index.js
+++ src/index.js
@@ -1,3 +1,55 @@
 import validator from './validator.js';
 
 console.log(validator);
+/*
+function capturado(){
+      let numerodetarjeta= document.getElementById("numtarjeta").value;
+      validator.isValid(numerodetarjeta)
+}
+document.getElementById("validar").addEventListener("click",capturado);*/
+document.getElementById("validar").addEventListener("click", ()=>{
+document.getElementById("pantallaUno").style.display="none";
+document.getElementById("pantallaDos").style.display="block";
+      
+//document.getElementById("cardNumbernumeral").innerHTML = numeroTarjeta;
+  
+const numeroTarjeta = document.getElementById("numtarjeta").value;
+let estadoTarjeta = true;
+let asterisco;
+//alert de ingreso de numero de tarjeta
+
+   
+  estadoTarjeta = validator.isValid(numeroTarjeta);
+  
+      if (numeroTarjeta.length == 0 ) {
+          alert("Debes llenar el campo");
+           
+           document.getElementById("validar").style.display="block";
+           
+        }
+  
+      else{
+  
+          if (estadoTarjeta == false) {
+          asterisco= validator.maskify(numeroTarjeta);
+          
+          
+          document.getElementById("validar").style.display="block";
+          document.getElementById("validar").style.display="block";
+          document.getElementById("cardNumberAsterisco").innerHTML ="NO VALIDO";
+          document.getElementById("numeroAsterisco").innerHTML =asterisco;
+          }
+    
+          else {
+              asterisco= validator.maskify(numeroTarjeta);
+              document.getElementById("validar").style.display="block";
+              document.getElementById("validar").style.display="block";
+              document.getElementById("cardNumberAsterisco").innerHTML ="VALIDO";
+              document.getElementById("numeroAsterisco").innerHTML =asterisco;
+
+           //volver   
+          }
+         }})
+document.getElementById("volver").addEventListener("click", ()=>{
+document.getElementById("pantallaDos").style.display="none";
+document.getElementById("pantallaUno").style.display="block";})
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,44 @@
 const validator = {
-  // ...
-};
-
+  isValid(numtarjeta){
+    console.log(numtarjeta);
+    //pasar los numeros a un array en orden inverso
+    let revertirNumero= Array.from(numtarjeta).reverse();
+    let suma = 0;
+    let estado = true;
+    for (let i=0; i<revertirNumero.length; i ++) {
+      //aplicar las operaciones a los numeros de las posiciones pares
+      if ((i+1) %  2 == 0) {
+        revertirNumero[i] =revertirNumero[i]*2;
+        //si es >=10, restar menos 9
+        if (revertirNumero[i]>9){
+        revertirNumero[i] =revertirNumero[i]-9;
+        }      
+      } 
+      //suma los digitos y nuevos digitos
+      suma+= parseInt(revertirNumero[i]);
+      console.log(revertirNumero); 
+    } 
+    //comprobar si es una tarjeta valida
+    if (suma%10==0) {
+      estado == true;
+    }
+    else{
+      estado=false;
+    }
+    return estado; 
+    console.log(suma);
+},
+  //enmascarar el número de la tarjeta excepto los ultimos 4 digitos  
+maskify : (numtarjeta)=>{
+    let num = Array. from(numtarjeta);
+      for (let i=0; i<=num.length; i++) {
+          if(i<num.length-4){
+            num[i]="#";
+          }
+        }
+        return num.join("");
+    
+    }
+}; 
+     
 export default validator;
